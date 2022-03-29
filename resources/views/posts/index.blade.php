b0464ba0ef9c05e8ee36d11a19b352b1e220c7f1
Macharia Mungai <kimanim68@gmail.com>
2022-03-29 09:07:12 +0300

    fixing the relationship

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,7 +11,7 @@
                     </div>
                     <div class="col-md-8 col-sm-8">
                         <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                        <small>Written on {{$post->created_at}}</small>
+                        <small>Written on {{$post->created_at}} by {{$post->user_id}}</small>
                     </div>
                 </div>
             </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
     {!!$post->body!!}
 </div>
 <hr>
-<small>Written on {{$post->created_at}} </small>
+<small>Written on {{$post->created_at}} by {{$post->user_id}}</small>
 <hr>
 @if(!Auth::guest())
     @if(Auth::user()->id == $post->user_id)
